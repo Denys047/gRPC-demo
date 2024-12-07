0c29deb143b2e409ae8c0ff8f6bb8146f8f452b7
Denys <dendet94@gmail.com>
2024-12-07 20:22:27 +0300

    example stub grpc

--- src/main/java/com/example/GrpcClient.java
+++ src/main/java/com/example/GrpcClient.java
@@ -1,19 +1,51 @@
 package com.example;
 
 import com.grpc.proto.service.user.UserRequest;
+import com.grpc.proto.service.user.UserResponse;
 import com.grpc.proto.service.user.UserServiceGrpc;
+import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.time.Duration;
 
 public class GrpcClient {
 
-    public static void main(String[] args) {
-        var channel = ManagedChannelBuilder.forAddress("localhost", 8082).usePlaintext().build();
+    private static final Channel channel = ManagedChannelBuilder.forAddress("localhost", 8082).usePlaintext().build();
+    private static final UserRequest userRequest = UserRequest.newBuilder().setId(1L).build();
 
-        UserServiceGrpc.UserServiceBlockingStub stub = UserServiceGrpc.newBlockingStub(channel);
+    public static void main(String[] args) throws InterruptedException {
+//        requestAsyncStub();
+//        requestBlockStub();
+    }
 
-        var response = stub.getUser(UserRequest.newBuilder().setId(2L).build());
+    public static void requestBlockStub() {
+        var blockStub = UserServiceGrpc.newBlockingStub(channel);
+        var userResponse = blockStub.getUser(userRequest);
+        System.out.println("block response: " + userResponse);
+    }
 
-        System.out.println(response.toString());
+    public static void requestAsyncStub() throws InterruptedException {
+        var asyncStub = UserServiceGrpc.newStub(channel);
+
+        asyncStub.getUser(userRequest, new StreamObserver<UserResponse>() {
+
+            @Override
+            public void onNext(UserResponse value) {
+                System.out.println("async response: " + value.toString());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("async stub done");
+            }
+        });
+        Thread.sleep(Duration.ofSeconds(5));
     }
 
 }
