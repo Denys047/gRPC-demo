8205b372666cf012ab11031a181e18fbde1b0873
Denys <dendet94@gmail.com>
2024-12-14 22:26:44 +0300

    clean project

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,9 @@
 package com.example;
 
-import com.example.repository.UserRepository;
-import com.example.server.GrpcServer;
-import com.example.service.UserService;
-
-import java.io.IOException;
-import java.util.List;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        var grpcServer = GrpcServer.createServer(8082, List.of(new UserService(new UserRepository())));
-        grpcServer.start();
+    public static void main(String[] args) {
+
     }
 
 }
